3581e4edbf48117f22a56be7779c866ae5a42b4d
alicansadeler <alicansadeler@gmail.com>
2024-09-11 16:26:03 +0300

    Transactional anatosyonu gerekli yerlere eklendi

--- src/main/java/com/alicansadeler/myecommerce/services/Impl/UserServiceImpl.java
+++ src/main/java/com/alicansadeler/myecommerce/services/Impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.alicansadeler.myecommerce.entity.User;
 import com.alicansadeler.myecommerce.exceptions.ApiException;
 import com.alicansadeler.myecommerce.repository.UserRepository;
 import com.alicansadeler.myecommerce.services.service.UserService;
+import com.alicansadeler.myecommerce.validations.UserValidation;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,7 +14,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 
 @Service
 public class UserServiceImpl implements UserDetailsService, UserService {
@@ -32,24 +33,40 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
+        UserValidation.validateUserForSave(user);
         return userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
+        UserValidation.validateId(id);
         User user = findById(id);
         userRepository.delete(user);
     }
 
     @Override
+    @Transactional
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
     @Override
     public User findById(Long id) {
+        UserValidation.validateId(id);
         return userRepository.findById(id)
-   .orElseThrow(() -> new ApiException("User not found with id: " + id, HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ApiException("ID not found, ID: " + id, HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    @Transactional
+    public User update(User user) {
+        UserValidation.validateUserForUpdate(user);
+        if (user.getId() == null || !userRepository.existsById(user.getId())) {
+            throw new ApiException("User not found", HttpStatus.NOT_FOUND);
+        }
+        return userRepository.save(user);
     }
 }
