234e6ee5a0ce476681aaab44f8aa2785553c0e3e
alicansadeler <alicansadeler@gmail.com>
2024-09-19 10:12:25 +0300

    dto price eklendi

--- src/main/java/com/alicansadeler/myecommerce/dto/response/ProductResponse.java
+++ src/main/java/com/alicansadeler/myecommerce/dto/response/ProductResponse.java
@@ -1,6 +1,7 @@
 package com.alicansadeler.myecommerce.dto.response;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public record ProductResponse(Long id, String name, String description, String imagePath, Long stockQuantity,String categoryName, List<ProductDetailResponse> productDetailResponse) {
+public record ProductResponse(Long id, String name, String description, String imagePath, Long stockQuantity, String categoryName, List<ProductDetailResponse> productDetailResponse, BigDecimal price) {
 }
